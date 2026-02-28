fef495dfa361ead6d1ffa21dae9eb5927a30d862
Matan Dizengof <diz.matan@gmail.com>
2026-02-28 11:08:47 +0200

    start grfics

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class main
+{
+    static void Main(string[] args)
+    {
+        
+    }
+
+
+    public static void show<thing>(thing erray)
+    {
+        foreach ((thing item ))
+    }
+}
